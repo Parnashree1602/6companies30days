89f3083ac860507f5c4e99a1856424badec630aa
Parnashree Das <94670642+Parnashree1602@users.noreply.github.com>
2023-02-04 08:35:07 +0530

    Time: 32 ms (36.03%), Space: 13.8 MB (61.90%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& p) {
+        vector<int> adj[n];
+        for(auto it:p)
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> in(n,0);
+        for(int i=0;i<n;i++)
+        {
+            for(auto it:adj[i])
+            {
+                in[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++)
+        {
+            if(in[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adj[node])
+            {
+                in[it]--;
+                if(in[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        vector<int> emp;
+        if(topo.size()==n)
+        {
+            return topo;
+        }
+        return emp;
+    }
+};
